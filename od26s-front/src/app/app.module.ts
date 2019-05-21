501729763acf5e039c07715c03460b6cfd8a39b9
Sendeskill <lucastsendeski@hotmail.com>
2019-05-21 18:42:23 -0300

    inclusao CKEditor 5

--- od26s-front/src/app/app.module.ts
+++ od26s-front/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {AccordionModule, AutoCompleteModule, ConfirmDialogModule, DialogModule, 
 import {TableModule} from 'primeng/table';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {DataViewModule} from 'primeng/dataview';
+import {CKEditorModule} from '@ckeditor/ckeditor5-angular';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import {DataViewModule} from 'primeng/dataview';
     TableModule,
     DataViewModule,
     AutoCompleteModule,
-    TabViewModule
+    TabViewModule,
+    CKEditorModule
   ],
   providers: [
     ConfirmationService,
