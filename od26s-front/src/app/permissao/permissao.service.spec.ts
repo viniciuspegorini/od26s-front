84e04f63061a401d2de0507efb4c3ada5252c0c5
Cleidisson <cleipd@gmail.com>
2019-06-03 21:53:37 -0300

    model , services permissao

---
+++ od26s-front/src/app/permissao/permissao.service.spec.ts
@@ -0,0 +1,14 @@
+import { TestBed } from '@angular/core/testing';
+
+import { PermissaoService } from './permissao.service';
+
+describe('PermissaoService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: PermissaoService = TestBed.get(PermissaoService);
+    expect(service).toBeTruthy();
+  });
+});
+
+
