3445df1a55413d19fa561a1dc42a83ce95c6e02a
Roberto Ramos <rtorms@gmail.com>
2019-06-25 21:10:56 -0300

    Ajuste layout com menu

--- od26s-front/src/app/services/amostra.service.ts
+++ od26s-front/src/app/services/amostra.service.ts
@@ -3,13 +3,19 @@ import { CrudService } from '../generic/crud.service';
 import { Amostra } from '../model/amostra';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import {Observable} from 'rxjs';
+import {Usuario} from '../model/usuario';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AmostraService extends CrudService<Amostra, Number> {
+export class AmostraService extends CrudService<Amostra, number> {
 
-  constructor(http: HttpClient) { 
+  constructor(http: HttpClient) {
     super(environment.api + '/amostra', http);
+  }
+  getLoggedUser(): Observable<Usuario> {
+    const url = `${environment.api}/logged-user`;
+    return this.http.get<Usuario>(url);
   }
 }
