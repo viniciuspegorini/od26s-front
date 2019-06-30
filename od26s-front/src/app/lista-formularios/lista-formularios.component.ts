1c9cda027a73e64867397d309dd30cfd0689278a
Willian Piantkoski <wjpiantkoski@gmail.com>
2019-06-30 14:42:15 -0300

    Novo componente lista-formularios

---
+++ od26s-front/src/app/lista-formularios/lista-formularios.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-formularios',
+  templateUrl: './lista-formularios.component.html',
+  styleUrls: ['./lista-formularios.component.css']
+})
+export class ListaFormulariosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
